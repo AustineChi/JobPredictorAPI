eef601548c69dff6024ef26b6b5bde33ba808577
Philip-21 <philipuzomaobiora@gmail.com>
2024-03-11 00:19:25 +0100

    token and jwt creation
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+// validates the token, extracts UserID and sets it in the request context
+func Auth() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		//validate token and exract userID
+		userID, err := ValidateToken(authHeader)
+		if err != nil {
+			log.Println("invalid or expired token:", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+		// Set the user ID in the request context
+		c.Set("userID", userID)
+
+		// Continue processing the request
+		c.Next()
+	}
+}
---
+++ middleware/jwt.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type jwtCustomClaims struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+var SECRET_KEY = os.Getenv("SECRET")
+
+// Generate JWT token with the claims and signing method.
+func GenerateToken(userID int, email string, admin bool) (string, error) {
+	claims := &jwtCustomClaims{
+		userID,
+		email,
+		admin,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	//create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	//generate token using secret key
+	t, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+	}
+	return t, nil
+
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Signing method validation
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		// Return the secret signing key
+		return []byte(SECRET_KEY), nil
+	})
+
+}
