26ad9dda5a64d1a52850e56b69e334fea9f49991
Philip-21 <philipuzomaobiora@gmail.com>
2024-03-11 05:41:28 +0100

    new modules, new handlers for login implemented
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- models/job.go
+++ models/job.go
@@ -7,32 +7,32 @@ import (
 
 // Job represents a job listing in the job search app
 type Job struct {
-	JobID          int     `gorm:"column:job_id;primary_key" json:"jobId"`
-	Title          string  `gorm:"column:title" json:"title"`
-	Description    string  `gorm:"column:description" json:"description"`
-	Company        string  `gorm:"column:company" json:"company"`
-	Location       string  `gorm:"column:location" json:"location"`
-	SkillsRequired string  `gorm:"column:skills_required" json:"skillsRequired"`
+	JobID          int    `gorm:"column:job_id;primary_key" json:"jobId"`
+	Title          string `gorm:"column:title" json:"title"`
+	Description    string `gorm:"column:description" json:"description"`
+	Company        string `gorm:"column:company" json:"company"`
+	Location       string `gorm:"column:location" json:"location"`
+	SkillsRequired string `gorm:"column:skills_required" json:"skillsRequired"`
 	Salary         BigInt `gorm:"column:salary" json:"salary"`
-	EmploymentType string  `gorm:"column:employment_type" json:"employmentType"`
+	EmploymentType string `gorm:"column:employment_type" json:"employmentType"`
 }
 
-//Implement a Valuer and Scanner interface for SQL to handle BigInt 
+// Implement a Valuer and Scanner interface for SQL to handle BigInt
 type BigInt struct {
 	big.Int
 }
 
 // Value converts BigInt to a database value
-func (bi BigInt)Value()(driver.Value, error){
+func (bi BigInt) Value() (driver.Value, error) {
 	return bi.String(), nil
 }
 
 // Scan converts a database value to BigInt
 func (bi *BigInt) Scan(value interface{}) error {
-    var s string
-    if value != nil {
-        s = string(value.([]byte))
-    }
-    bi.SetString(s, 10)
-    return nil
+	var s string
+	if value != nil {
+		s = string(value.([]byte))
+	}
+	bi.SetString(s, 10)
+	return nil
 }
--- models/user.go
+++ models/user.go
@@ -1,31 +1,43 @@
 package models
 
 import (
-    "golang.org/x/crypto/bcrypt"
-)
+	"errors"
+	"log"
 
+	"golang.org/x/crypto/bcrypt"
+)
 
 // User represents a user of the job search app
 type User struct {
-    UserID         int    `gorm:"column:user_id;primary_key" json:"userId"`
-    Name           string `gorm:"column:name" json:"name"`
-    Email          string `gorm:"column:email" json:"email"`
-    PasswordHash   string `gorm:"column:password" json:"-"`
-    JobPreferences string `gorm:"column:job_preferences" json:"jobPreferences"`
+	UserID         int    `gorm:"column:user_id;primary_key" json:"userId"`
+	Name           string `gorm:"column:name" json:"name"`
+	Email          string `gorm:"column:email" json:"email"`
+	PasswordHash   string `gorm:"column:password" json:"password"`
+	JobPreferences string `gorm:"column:job_preferences" json:"jobPreferences"`
 }
 
 // SetPassword sets a hashed password for a user
-func (u *User) SetPassword(password string) error {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    if err != nil {
-        return err
-    }
-    u.PasswordHash = string(bytes)
-    return nil
+func (u *User) SetPassword(password string) ([]byte, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return nil, err
+	}
+	//u.PasswordHash = string(bytes)
+	return bytes, nil
 }
 
 // CheckPassword compares a provided password with the user's hashed password
-func (u *User) CheckPassword(password string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
-    return err == nil
+func (u *User) CheckPassword(password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			log.Println("Invalid Password")
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	log.Println("password validated")
+	return true, nil
 }
